accbfebffc90826be2171960b3a72bfdb99f751a
Barrett <onkoe@protonmail.com>
2024-02-02 15:44:08 -0600

    fix: Use `spi::Error` trait in `error.rs`

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,18 @@
+use embedded_hal::spi;
+
 /// Some problem with the MAX6675 or its connections
 #[derive(Copy, Clone, PartialEq, PartialOrd)]
-pub enum Max6675Error<SpiError, CsError> {
+pub enum Max6675Error<SpiError: spi::Error> {
     SpiError(SpiError),
-    CsError(CsError),
     OpenCircuitError,
 }
 
-impl<SpiError, CsError> Max6675Error<SpiError, CsError> {
+impl<SpiError: spi::Error> Max6675Error<SpiError> {
     fn message(&self) -> &'static str {
         match *self {
             Max6675Error::SpiError(_) => {
                 "An error occured while attempting to reach the MAX6675 over SPI."
             }
-            Max6675Error::CsError(_) => "The MAX6675 has encountered a chip select pin error.",
             Max6675Error::OpenCircuitError => {
                 "The MAX6675 detected an open circuit (bit D2 was high). \
                 Please check the thermocouple connection and try again."
@@ -22,20 +22,20 @@ impl<SpiError, CsError> Max6675Error<SpiError, CsError> {
 }
 
 // implicit `?` syntax for SpiError to Max6675Error
-impl<SpiError, CsError> core::convert::From<SpiError> for Max6675Error<SpiError, CsError> {
+impl<SpiError: spi::Error> core::convert::From<SpiError> for Max6675Error<SpiError> {
     fn from(value: SpiError) -> Self {
         Max6675Error::SpiError(value)
     }
 }
 
 // print... if you must
-impl<SpiError, CsError> core::fmt::Display for Max6675Error<SpiError, CsError> {
+impl<SpiError: spi::Error> core::fmt::Display for Max6675Error<SpiError> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{}", self.message())
     }
 }
 
-impl<SpiError, CsError> ufmt::uDisplay for Max6675Error<SpiError, CsError> {
+impl<SpiError: spi::Error> ufmt::uDisplay for Max6675Error<SpiError> {
     fn fmt<W>(&self, f: &mut ufmt::Formatter<'_, W>) -> Result<(), W::Error>
     where
         W: ufmt::uWrite + ?Sized,
@@ -45,13 +45,13 @@ impl<SpiError, CsError> ufmt::uDisplay for Max6675Error<SpiError, CsError> {
 }
 
 // debug impls
-impl<SpiError, CsError> core::fmt::Debug for Max6675Error<SpiError, CsError> {
+impl<SpiError: spi::Error> core::fmt::Debug for Max6675Error<SpiError> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{}", self.message())
     }
 }
 
-impl<SpiError, CsError> ufmt::uDebug for Max6675Error<SpiError, CsError> {
+impl<SpiError: spi::Error> ufmt::uDebug for Max6675Error<SpiError> {
     fn fmt<W>(&self, f: &mut ufmt::Formatter<'_, W>) -> Result<(), W::Error>
     where
         W: ufmt::uWrite + ?Sized,
@@ -63,9 +63,7 @@ impl<SpiError, CsError> ufmt::uDebug for Max6675Error<SpiError, CsError> {
 // implement error if it's feasible
 // FIXME: use core::error::Error once stable! <3
 #[cfg(feature = "std")]
-impl<SpiError: std::fmt::Debug, CsError: std::fmt::Debug> std::error::Error
-    for Max6675Error<SpiError, CsError>
-{
+impl<SpiError: embedded_hal::spi::Error> std::error::Error for Max6675Error<SpiError> {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         None // other types typically don't impl error :p
     }
